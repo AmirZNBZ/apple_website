b1514d4dd5731096438d0c2bec8d0f30bb7145a3
amir nabiizade <amirnabiizade@gmail.com>
2025-07-06 16:38:23 +0330

    comment base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/apple_website/",
+  // base: "/apple_website/",
   plugins: [
     react(),
     sentryVitePlugin({
